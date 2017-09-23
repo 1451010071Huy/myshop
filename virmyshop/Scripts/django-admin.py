62df70a8f7bdec69c0fb33dc784251d85a2e7468
Huy <1451010071Huy@ou.edu.vn>
2017-09-23 12:26:55 +0700

    my shop

---
+++ virmyshop/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Luxury\Desktop\New folder (2)\Ex_Files_Django_3_Online_Shop\Exercise Files\Chapter 1\virmyshop\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ virmyshop/Scripts/gifmaker.py
@@ -0,0 +1,31 @@
+#!C:\Users\Luxury\Desktop\New folder (2)\Ex_Files_Django_3_Online_Shop\Exercise Files\Chapter 1\virmyshop\Scripts\python.exe
+#
+# The Python Imaging Library
+# $Id$
+#
+# convert sequence format to GIF animation
+#
+# history:
+#       97-01-03 fl     created
+#
+# Copyright (c) Secret Labs AB 1997.  All rights reserved.
+# Copyright (c) Fredrik Lundh 1997.
+#
+# See the README file for information on usage and redistribution.
+#
+
+from __future__ import print_function
+
+from PIL import Image
+
+if __name__ == "__main__":
+
+    import sys
+
+    if len(sys.argv) < 3:
+        print("GIFMAKER -- create GIF animations")
+        print("Usage: gifmaker infile outfile")
+        sys.exit(1)
+
+    im = Image.open(sys.argv[1])
+    im.save(sys.argv[2], save_all=True)
