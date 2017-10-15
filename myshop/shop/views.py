1ded1a2c41329c08cd53741b5b768675d10cdf3e
Nguyen Manh Khang <1451010085khang@ou.edu.vn>
2017-10-15 21:19:27 +0700

    initiaze Search, add link to profile

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -2,14 +2,19 @@ from django.shortcuts import render, get_object_or_404, render_to_response
 from .models import Category, Product
 from cart.forms import CartAddProductForm
 from django.http import HttpResponseRedirect
+from .models import Product
 
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
+
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
+    query = request.GET.get("search")
+    if query:
+        products = products.filter(name__icontains=query)
     return render(request, 'shop/product/list.html', {'category': category,
                                                       'categories': categories,
                                                       'products': products})
@@ -18,7 +23,12 @@ def product_list(request, category_slug=None):
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
     cart_product_form = CartAddProductForm()
+
     return render(request, 'shop/product/detail.html',
                         {'product': product,
                          'cart_product_form': cart_product_form})
+
+
+
+
 
