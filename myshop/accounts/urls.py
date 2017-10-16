7c080d9f3b6561fcc33052ca026abd3926eb4365
Nguyen Manh Khang <1451010085khang@ou.edu.vn>
2017-10-16 20:48:55 +0700

    modify some stuff

--- myshop/accounts/urls.py
+++ myshop/accounts/urls.py
@@ -1,3 +1,3 @@
 from django.conf.urls import url
-from . import views
+from .views import logins, logout_view, register_user_view, view_profile, edit_profile
 
--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -2,13 +2,13 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from accounts.views import logins, logout_view, register_user_view, view_profile, edit_profile
+from accounts.views import logins, logout_view, register_user_view, view_profile, edit_profile,change_password
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^cart/', include('cart.urls', namespace='cart')),
     url(r'^orders/', include('orders.urls', namespace='orders')),
-   ## url(r'^accounts/', include('accounts.urls', namespace='accounts')),
+
 
     ##user auth urls
     url(r'^accounts/login/$', logins, name='logins'),
@@ -16,6 +16,7 @@ urlpatterns = [
     url(r'^accounts/register/$', register_user_view, name='register'),
     url(r'^accounts/profile/$', view_profile, name='profile'),
     url(r'^accounts/profile/edit/$', edit_profile, name='edit_profile'),
+    url(r'^accounts/profile/change-password/$', change_password, name='change_password'),
     url(r'^', include('shop.urls', namespace='shop')),
 
 
