46f47d0033464f544879cc530d0e867eb3ae37b0
Huy <1451010071Huy@ou.edu.vn>
2017-10-19 02:54:30 +0700

    fix

--- virmyshop/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Luxury\Desktop\New folder (2)\Ex_Files_Django_3_Online_Shop\Exercise Files\Chapter 1\virmyshop\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
